0276da8724ff123c0548c4c2fd8f88765909fda8
Don Booth <donbooth@rogers.com>
2021-08-16 14:31:25 -0400

    initial commit.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ dotenv.config( { path: './config/config.env' } )
 
 const app = express()
 
+// Routes
+app.get('/', (req, res) =>{
+    res.status(200).json({success: true })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`))
